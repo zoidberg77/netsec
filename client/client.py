027f6dd7e2d81978ec0773f181f2ebbfd6e9a2c6
sidakk95 <kevin.sidak@univie.ac.at>
2021-04-02 16:27:56 +0200

    fin

--- client/client.py
+++ client/client.py
@@ -4,11 +4,10 @@ import requests
 import pickle
 import base64
 
-data = pd.read_csv('../malicious-connection-dataset/df_bad.csv').head(5)
-data2 = pd.read_csv('../malicious-connection-dataset/df_good.csv').head(5)
-#data = pd.concat(data1, data2)
+data1 = pd.read_csv('../malicious-connection-dataset/df_bad.csv').head(10)
+data = pd.concat([data1])
 pickled = pickle.dumps(data)
 pickled_b64 = base64.b64encode(pickled)
 hug_pickled_str = pickled_b64.decode('utf-8')
-result = requests.request(method='get', url='http://localhost:5000/predict', data=hug_pickled_str);
+result = requests.request(method='get', url='http://localhost:5000/predict', data=hug_pickled_str)
 print(result.content)
